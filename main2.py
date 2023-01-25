ab635733b98f60a56321c08fbce92099a47ac5c5
Eder Dalpizzol <dalpizzol.eder@gmail.com>
2023-01-25 17:21:22 -0300

    .

---
+++ main2.py
@@ -0,0 +1,37 @@
+import PySimpleGUIWeb as sg
+
+import cadprod, listprod, sales, create_db
+
+create_db.db()
+
+sg.theme('LightBlue')
+
+layout = [
+    [sg.Text('SysVenda')],
+    [sg.Button('Cadastrar Produto', key='add_product', expand_x=True)],
+    [sg.Button('Consultar Produtos', key='view_products', expand_x=True)],
+    [sg.Button('Inserir Venda', key='add_sale', expand_x=True)],
+    [sg.Text('')],
+    [sg.Text('')],
+    [sg.Text('')],
+    [sg.Button('Sair', expand_x=True)]
+]
+
+window = sg.Window.create('SysVenda', layout, size=(600, 245), finalize=True, web_start_browser=True)
+
+while True:
+    event, values = window.Read()
+
+    if event == 'add_product':
+        cadprod.cad_prod()
+        
+    if event == 'view_products':
+        listprod.view_products()
+
+    if event == 'add_sale':
+        sales.sales()
+
+    if event == 'Sair' or event is None:
+        break
+
+window.Close()
